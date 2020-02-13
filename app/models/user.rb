29aaa5bc942998f1fc31887fafd43d8771fc3fc0
Ermin Cahtarevic <erminc@windowslive.com>
2020-02-13 18:55:42 +0100

    Updated cover image uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
   validates :full_name, presence: true
   validate :photo_size
+  validate :cover_size
 
   mount_uploader :photo, PhotoUploader
   mount_uploader :cover_image, CoverImageUploader
@@ -15,6 +16,12 @@ class User < ApplicationRecord
     def photo_size
       if photo.size > 5.megabytes
         errors.add(:photo, "should be less than 5MB")
+      end
+    end
+
+    def cover_size
+      if cover_image.size > 5.megabytes
+        errors.add(:cover_image, "should be less than 5MB")
       end
     end
 end
--- app/uploaders/cover_image_uploader.rb
+++ app/uploaders/cover_image_uploader.rb
@@ -1,7 +1,7 @@
 class CoverImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
@@ -38,9 +38,9 @@ class CoverImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
