385786b16b7158dfced2116afb05e30be4115d47
Ermin Cahtarevic <erminc@windowslive.com>
2020-02-19 18:01:31 +0100

    Update the opinions helper, removing the comments

--- app/helpers/opinions_helper.rb
+++ app/helpers/opinions_helper.rb
@@ -1,8 +1,5 @@
 module OpinionsHelper
   def following_opinions
-    #users = User.includes(:opinions, :followings).where(followings: current_user.id)
-    #Opinion.where('user_id IN (?)', users).order(created_at: :desc)
-    ids = current_user.followings.pluck(:followed_id) << current_user.id
     @opinions = Opinion.following_opinions(current_user)
   end
 end
